64e0f9ad67c3debdf17f00723cdf321c803d64c1
Abhansh <abhanshonparkhe@gmail.com>
2022-11-29 12:21:53 +0530

    initial commit

---
+++ app/src/main/java/com/gsr/sudoku/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gsr.sudoku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(), v.getId() + " clicked", Toast.LENGTH_SHORT).show();
+    }
+}
